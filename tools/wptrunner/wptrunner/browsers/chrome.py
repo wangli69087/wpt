5c4d89ee214787583cb9d569531ccd945e0725b1
Stephen McGruer <smcgruer@chromium.org>
2020-09-09 07:57:33 -0400

    [Testing] Drop --ignore-certificate-errors for Chrome
    As per crbug.com/1125272, --ignore-certificate-errors is deprecated for
    Chrome, so lets see if we can drop it (or more precisely, what breaks
    when we do!).

--- tools/wptrunner/wptrunner/browsers/chrome.py
+++ tools/wptrunner/wptrunner/browsers/chrome.py
@@ -87,7 +87,6 @@ def executor_kwargs(test_type, server_config, cache_manager, run_info_data,
     # browsing context, whereas the CLI flag works for workers, too.
     chrome_options["args"] = []
 
-    chrome_options["args"].append("--ignore-certificate-errors")
     chrome_options["args"].append("--ignore-certificate-errors-spki-list=%s" %
                                   ','.join(IGNORE_CERTIFICATE_ERRORS_SPKI_LIST))
 
